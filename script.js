1ed9191cedd3e3ab0ff2b9fdeff0845ddeda92ea
Sushan <sushan.rayamajhi7@gmail.com>
2026-09-25 21:44:44 +0545

    JS done

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 (function () {
-  var requestAnimationFrame =
+  const requestAnimationFrame =
     window.requestAnimationFrame ||
     window.mozRequestAnimationFrame ||
     window.webkitRequestAnimationFrame ||
     window.requestAnimationFrame;
+
   window.requestAnimationFrame = requestAnimationFrame;
 })();
 
@@ -53,7 +54,7 @@ powers.push({
   width: 20,
   height: 20,
   color: "#222",
-  effect: "tele",
+  effect: "teleport",
   rotate: 20,
   px: 20,
   py: 370,
@@ -75,84 +76,84 @@ boxes.push({
   y: height / 4 + 10,
   width: 10,
   height: height,
-  color: "green",
+  color: 'green'
 });
 boxes.push({
   x: 0,
   y: 0,
   width: 10,
   height: height / 4 - 15,
-  color: "green",
+  color: 'green'
 });
 boxes.push({
   x: 0,
   y: height - 10,
   width: width,
   height: 50,
-  color: "orange",
+  color: 'orange'
 });
 boxes.push({
   x: width - 10,
   y: 0,
   width: 50,
   height: height,
-  color: "yellow",
+  color: 'yellow'
 });
 boxes.push({
   x: 290,
   y: 200,
   width: 260,
   height: 10,
-  color: "blue",
+  color: 'blue'
 });
 boxes.push({
   x: 590,
   y: 200,
   width: 80,
   height: 10,
-  color: "blue",
+  color: 'blue'
 });
 boxes.push({
   x: 120,
   y: 250,
   width: 150,
   height: 10,
-  color: "red",
+  color: 'red'
 });
 boxes.push({
   x: 220,
   y: 300,
   width: 80,
   height: 10,
-  color: "black",
+  color: 'black'
 });
 boxes.push({
   x: 340,
   y: 350,
   width: 90,
   height: 10,
-  color: "#655643",
+  color: '#655643'
 });
 boxes.push({
   x: 740,
   y: 300,
   width: 160,
   height: 10,
-  color: "#655543",
+  color: '#655543'
 });
 boxes.push({
   x: 0,
   y: 350,
   width: 90,
   height: 10,
-  color: "#655543",
+  color: '#655543'
 });
 boxes.push({
   x: 90,
   y: 350,
   width: 10,
   height: 50,
-  color: "#655543",
+  color: '#655543'
 });
 
 canvas.width = width;
@@ -168,7 +169,6 @@ function update() {
     player.grounded = false;
     player.velY = -player.speed * 2.5;
   }
-}
 if ((keys[39] || keys[68]) && player.velX < player.speed) {
   player.velX++;
 }
@@ -211,6 +211,82 @@ player.y += player.velY;
 ctx.fill();
 ctx.fillStyle = player.color;
 ctx.fillRect(player.x, player.y, player.width, player.height);
+
+for (let j = 0; j < powers.length; j++){
+  ctx.save();
+  var cx = powers[j].x + 0.5 * powers[j].width,
+  cy = powers[j].y + 0.5 * powers[j].height;
+  ctx.translate(cx, cy);
+  ctx.rotate((Math.PI / 180) * 45);
+  if(powers[j].effect === 'teleport'){
+    ctx.rotate ( (Math.PI / 180) * powers[j].rotate);
+    powers[j].rotate = (Math.PI / 180) * powers[j].rotate;
+  }
+  ctx.translate(-cx, -cy);
+  ctx.fillStyle = powers[j].color;
+  ctx.fillRect(powers[j].x, powers[j].y, powers[j].width, powers[j].height);
+  ctx.restore();
+
+if(colCheck(player, powers[j]) !==null){
+  if(powers[j].effect==='gravity'){
+    gravity= 0.4;
+    player.speed = 4;
+    player.color= 'white';
+  }
+
+  else if (powers[j].effect==='shrink'){
+    player.width =10;
+    player.height= 10;
+    player.speed=5;
+  }
+  else if (powers[j].effect==='teleport'){
+    player.x=powers[j].px;
+    player.y=powers[j].py;
+  }
+  else if (powers[j].effect==='win'){
+    const r = confirm("You win! Play again?");
+    if (r==false){
+      player.x=200;
+      player.y=200;
+    } else {
+      window.location.reload();
+    }
+  }
+  if(powers[j].stay!==true)
+    powers[j].width=0;
+}
+}
+requestAnimationFrame(update);
+}
+
+function colCheck(shapeA, shapeB) {
+  var vX = (shapeA + (shapeA.width / 2)) - (shapeB.x + (shapeB.width / 2)),
+        vY = (shapeA + (shapeA.height / 2)) - (shapeB.y + (shapeB.height / 2)),
+        hWidths = (shapeA.width / 2) + (shapeB.width / 2),
+        hHeights = (shapeA.width / 2)+ (shapeB.height / 2),
+        colDir = null;
+
+  if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {
+    var oX = hWidths - Math.abs(vX),
+          oY = hHeights - Math.abs(vY);
+    if (oX >= oY) {
+      if (vY > 0) {
+        colDir = 't';
+        shapeA.y += oY;
+      } else {
+        colDir = "b";
+        shapeA.y -= oY;
+      }
+    } else if (vX > 0) {
+      colDir = "l";
+      shapeA.x += oX;
+    } else {
+      colDir = "r";
+      shapeA.x -= oX;
+    }
+  }
+  return colDir;
+}
 
 document.body.addEventListener("keydown", function (e) {
   keys[e.keyCode] = true;
